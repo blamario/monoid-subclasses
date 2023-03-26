50a43aa7e23058a424c839256549fe5677db7784
Jonathan Knowles <mail@jonathanknowles.net>
2023-03-26 08:56:02 +0000

    Add `DistributiveLCMMonoid` class and instances.

--- src/Data/Monoid/LCM.hs
+++ src/Data/Monoid/LCM.hs
@@ -9,17 +9,18 @@
 --
 -- The 'LCMMonoid' class is for Abelian, /i.e./, 'Commutative' monoids.
 --
-module Data.Monoid.LCM (
-    LCMMonoid (..)
+module Data.Monoid.LCM
+    ( LCMMonoid (..)
+    , DistributiveLCMMonoid
     )
-where
+    where
 
 import Prelude hiding (gcd, lcm, max)
 import qualified Prelude
 
 import Data.IntSet (IntSet)
 import Data.Monoid (Dual (..), Product (..), Sum (..))
-import Data.Monoid.GCD (GCDMonoid (..))
+import Data.Monoid.GCD (GCDMonoid (..), DistributiveGCDMonoid)
 import Data.Set (Set)
 import Numeric.Natural (Natural)
 import qualified Data.IntSet as IntSet
@@ -29,6 +30,10 @@ import qualified Data.Set as Set
 import Data.Maybe (isJust)
 import Data.Semigroup.Cancellative (Reductive ((</>)))
 import Data.Semigroup.Commutative (Commutative)
+
+--------------------------------------------------------------------------------
+-- LCMMonoid
+--------------------------------------------------------------------------------
 
 -- | Class of Abelian monoids that allow the /least common multiple/ to be
 --   found for any two given values.
@@ -141,3 +146,40 @@ instance (LCMMonoid a, LCMMonoid b, LCMMonoid c, LCMMonoid d) =>
   where
     lcm (a0, a1, a2, a3) (b0, b1, b2, b3) =
         (lcm a0 b0, lcm a1 b1, lcm a2 b2, lcm a3 b3)
+
+--------------------------------------------------------------------------------
+-- DistributiveLCMMonoid
+--------------------------------------------------------------------------------
+
+-- | Class of /commutative/ LCM monoids with /distributivity/.
+--
+-- In addition to the general 'LCMMonoid' laws, instances of this class
+-- must also satisfy the following laws:
+--
+-- The 'lcm' operation itself must be /both/ left-distributive /and/
+-- right-distributive:
+--
+-- @
+-- 'lcm' (a '<>' b) (a '<>' c) '==' a '<>' 'lcm' b c
+-- @
+-- @
+-- 'lcm' (a '<>' c) (b '<>' c) '==' 'lcm' a b '<>' c
+-- @
+--
+-- The 'lcm' and 'gcd' operations must distribute over one another:
+--
+-- @
+-- 'lcm' a ('gcd' b c) '==' 'gcd' ('lcm' a b) ('lcm' a c)
+-- @
+-- @
+-- 'gcd' a ('lcm' b c) '==' 'lcm' ('gcd' a b) ('gcd' a c)
+-- @
+--
+class (DistributiveGCDMonoid m, LCMMonoid m) => DistributiveLCMMonoid m
+
+instance DistributiveLCMMonoid ()
+instance DistributiveLCMMonoid (Product Natural)
+instance DistributiveLCMMonoid (Sum Natural)
+instance DistributiveLCMMonoid IntSet
+instance Ord a => DistributiveLCMMonoid (Set a)
+instance DistributiveLCMMonoid a => DistributiveLCMMonoid (Dual a)
